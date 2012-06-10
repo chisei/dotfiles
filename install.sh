6b3db3452f7389511a264abb99801a5bb04cfd59
Chisei Takenouchi <chisei_takenouchi@voyagegroup.com>
2012-06-10 20:53:13 +0900

    install.shは動作が不完全であるためベタ書きにした。

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # install bashrc
 
-CURRENT_DIR=${0%%install.sh}
 BASHRC=~/.bashrc
 
-if [ -e $BASHRC ]; then
 # file exists
-    echo "source $CURRENT_DIR/.bashrc" >> $BASHRC
-else
-# file not exists
-    cp $CURRENT_DIR/.bashrc ~/
-fi
+echo "source ~/dotfiles/.bashrc" >> $BASHRC
