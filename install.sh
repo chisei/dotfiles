2ea8b460af04dc87157e62643876bb4a9508a0d6
Chisei Takenouchi <chisei.takenouchi@gmail.com>
2013-07-14 23:05:43 +0900

    add ln -s .grc

--- install.sh
+++ install.sh
@@ -16,3 +16,4 @@ ln -s $WORK_SPACE/.zshrc $HOME/.zshrc
 ln -s $WORK_SPACE/.vimrc $HOME/.vimrc
 ln -s $WORK_SPACE/.tmux.conf $HOME/.tmux.conf
 ln -s $WORK_SPACE/.my.cnf $HOME/.my.cnf
+ln -s $WORK_SPACE/.grc $HOME/.grc
