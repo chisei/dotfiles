a2bedb53c396cb0714b40ccb4efdee45edfd0ec0
Chisei Takenouchi <chisei.takenouchi@gmail.com>
2013-07-22 17:30:13 +0900

    fix vim install

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ cd $WORK_SPACE
 git submodule init
 git submodule update
 
-# vim NeoBundleInstall
-vim -c "NeoBundleInstall" -c "quit"
-
 # make symbolic link
 cd $HOME
 ln -s $WORK_SPACE/.zshrc $HOME/.zshrc
+ln -s $WORK_SPACE/.vim $HOME/.vim
 ln -s $WORK_SPACE/.vimrc $HOME/.vimrc
 ln -s $WORK_SPACE/.tmux.conf $HOME/.tmux.conf
 ln -s $WORK_SPACE/.my.cnf $HOME/.my.cnf
 ln -s $WORK_SPACE/.grc $HOME/.grc
+
+# vim NeoBundleInstall
+vim -c "NeoBundleInstall" -c "quit"
